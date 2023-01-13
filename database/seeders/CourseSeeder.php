1e3ff58a34d624af031a16a6b9ce056a9589c27a
sebasttti <juansjoya_r@outlook.com>
2023-01-12 20:39:41 -0500

    seeders

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Course;
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+            
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $course = new Course();
+        $course->name = 'Laravel';
+        $course->description = 'Curso de Laravel';
+        $course->category = 'Desarrollo Web';
+        
+        $course->save();
+    }
+}
